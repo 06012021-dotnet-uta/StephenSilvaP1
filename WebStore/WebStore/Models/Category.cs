23f0786f96f90b14c45822c7e0c264c43c579a35
ssilva2021 <stephen.silva@raveture.net>
2021-06-27 14:52:58 -0400

    Code first db migration example

---
+++ WebStore/WebStore/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    public class Category
+    {
+        //[key]  // if you use CategoryID that will also denot that this column is primary key
+        public int CategoryId { get; set; }
+        public String Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
